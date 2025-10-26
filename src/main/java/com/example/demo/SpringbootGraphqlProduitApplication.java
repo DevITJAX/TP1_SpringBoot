3a4b25338e3ac9c1afe188dc83f34700dab9ee5d
DevITJAX <devitjax@users.noreply.github.com>
2025-10-26 21:38:43 +0100

    TP1 : SPRINGBOOT/GRAPHQL

---
+++ src/main/java/com/example/demo/SpringbootGraphqlProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlProduitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraphqlProduitApplication.class, args);
+    }
+
+}
